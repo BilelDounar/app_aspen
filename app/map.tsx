f835b664e22ab9563285abec4fda2d90217ae9ab
BilelDounar <bileldounar@gmail.com>
2026-02-13 14:47:09 +0100

    fix: mise a jour du layout

--- app/map.tsx
+++ app/map.tsx
@@ -1,11 +1,10 @@
 import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-export default function map() {
+export default function Map() {
     return (
-        <View className="flex-1">
+        <View className="flex-1 bg-red-500 mt-8">
             <Text className="text-2xl font-bold">Map</Text>
-
         </View>
-
     );
-};
+}
