766ef5a56686d22cfcad412c0f39d591b6f95ec0
BilelDounar <bileldounar@gmail.com>
2026-02-13 14:29:02 +0100

    fix: layout & css

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,11 @@
-export default function _layout() {
-    
-    return null;
+import './global.css';
+import { Tabs } from "expo-router";
+
+export default function RootLayout() {
+    return <Tabs>
+        <Tabs.Screen name="index" options={{ title: "Home" }} />
+        <Tabs.Screen name="about" options={{ title: "About" }} />
+        <Tabs.Screen name="contact" options={{ title: "Contact" }} />
+        <Tabs.Screen name="news" options={{ title: "News", headerShown: false }} />
+    </Tabs>;
 };
